2a00f9f5782fdc768248c99802a6c70b1bfdc087
haripriyat <t.haripriya94@gmail.com>
2016-11-11 12:23:57 +0530

    hpbranch:book,booklet,inbook,masterthesis,manual,conference,unpublished

--- app/bib-create/bib-add/bib.js
+++ app/bib-create/bib-add/bib.js
@@ -10,7 +10,13 @@ angular.module('mlrg.bibcreate')
 		var availableTypes = [
 			{id: '1', name: 'Article'},
 			{id: '2', name: 'InProceedings'},
-			{id: '3', name: 'InBook'}
+			{id: '3', name: 'InBook'},
+			{id: '4', name: 'MasterThesis'},
+			{id: '5', name: 'Unpublished'},
+			{id: '6', name: 'Manual'},
+			{id: '7', name: 'Book'},
+			{id: '8', name: 'Booklet'},
+			{id: '9', name: 'Conference'},
 		];
 
 		var selectedType = {id: '1', name: 'Article'};
@@ -18,8 +24,10 @@ angular.module('mlrg.bibcreate')
 
 		var articleFields = ['author','title','journal','year','doi','abstract','volume','pages','note','number','month','crossref','keywords','file','url','comment','owner','timestamp'];
 		var inProceedingsFields = ['author','title','book','year','abstract'];
-		var inBookFields = [];
-
+		var inBookFields = ['Chapter','Pages','Title','Publisher','Year','Author','Editor','DOI','Abstract','Volume','Series','Address','Month','Number','Type','Edition','Note','Crossref','Keywords','File','URL','Comment','Owner','TimeStamp'];
+		var masterTheisis = [];
+		var unpublished = [];
+		var manual = [];
 
 		var bibFields = {
 			metadata: {
@@ -98,13 +106,32 @@ angular.module('mlrg.bibcreate')
 					makeOtherFieldsEmpty(data, articleFields);
 					break;
 				}
-
 				case 'inproceedings': {
 					makeOtherFieldsEmpty(data,inProceedingsFields);
 					break;
 				}
 				case 'inbook': {
 					makeOtherFieldsEmpty(data, inBookFields);
+					break;
+				}
+				case 'masterThesis': {
+					makeOtherFieldsEmpty(data, inmasterThesisFields);
+					break;
+				}
+				case 'manual': {
+					makeOtherFieldsEmpty(data, inmanualFields);
+					break;
+				}
+				case 'book': {
+					makeOtherFieldsEmpty(data, inbookFields);
+					break;
+				}
+				case 'booklet': {
+					makeOtherFieldsEmpty(data, inbookletFields);
+					break;
+				}
+				case 'conference': {
+					makeOtherFieldsEmpty(data, inconferenceFields);
 					break;
 				}
 			}
