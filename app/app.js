c1d9b5a982c84b9f5544c9264b68851d006a9510
Dhruv Prakash <dhruvprakash@Dhruvs-MacBook-Air.local>
2016-11-30 13:42:01 +0530

    upload file feature

--- app/app.js
+++ app/app.js
@@ -3,11 +3,13 @@
 var angular = require('angular');
 require('angular-ui-router');
 require('angular-ui-bootstrap');
-
+require('sweetalert');
+require('angular-sweetalert');
 var states = require('./states');
 require('./home');
 require('./bib-create');
-var app = angular.module('mlrg',['ui.bootstrap','ui.router','mlrg.home','mlrg.bibcreate']);
+require('./directives');
+var app = angular.module('mlrg',['ui.bootstrap','ui.router','mlrg.home','mlrg.bibcreate','mlrg.directives','oitozero.ngSweetAlert']);
 
 app.config(states);
 
