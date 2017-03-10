21338d068195146114dd83579125f9a11ec20e8e
Dhruv Prakash <dhruvprakash@Dhruvs-MacBook-Air.local>
2017-03-11 01:44:56 +0530

    log

--- app/home/home.ctrl.js
+++ app/home/home.ctrl.js
@@ -9,6 +9,8 @@ HomeController.$inject = ['$cookies','$scope'];
 function HomeController($cookies, $scope) {
 	var cookie = $cookies.get('user_details');
 	cookie = cookie.replace(/\\054/g, ',');
+	console.log(cookie);
+	console.log(JSON.parse(cookie));
 	$scope.userInfo = {
 		userName: JSON.parse(cookie).username,
 		group: JSON.parse(cookie).group
