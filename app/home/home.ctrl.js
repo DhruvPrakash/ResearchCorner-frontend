f9370a0ca560cb8cd3c769d06ab8430ae62162ea
Dhruv Prakash <dhruvprakash@Dhruvs-MacBook-Air.local>
2017-04-12 21:43:47 +0530

    comments added to explain this convoluted cookie mess

--- app/home/home.ctrl.js
+++ app/home/home.ctrl.js
@@ -7,47 +7,36 @@ HomeController.$inject = ['$cookies','$scope'];
 
 
 function HomeController($cookies, $scope) {
-	//var cookie = $cookies.getObject('user_details');
-	var cookie1 = $cookies.get('user_details');
-	cookie1 = cookie1.replace(/\\054/g, ',');
-	var splitOn = cookie1.split(',');
-	//console.log('split on:');
-	//console.log(splitOn);
-	var result1 = splitOn[0].slice(1) + '}';
-	result1 = result1.replace(/\\/g, '');
-	var result2 = '{' + splitOn[1].slice(1, -1);
-	result2 = result2.replace(/\\/g, '');
 	
-	result1 = result1.slice(1,-1);
-	result2 = result2.slice(1,-1);
-	console.log(result1);
-	console.log(result2);
-
-	var result1parts = result1.split(':');
-	var result2parts = result2.split(':');
-
+	/*-----This code here is to parse the cookie set by the server. A simple JSON.parse does not work which is why i have done this convoluted thing*/
+	var cookie = $cookies.get('user_details');
+	cookie = cookie.replace(/\\054/g, ','); // make that 054 into a ,
+	
+	var splitOn = cookie.split(',');
+	var beforecomma = splitOn[0].slice(1) + '}'; //Add a } to make the first part look like an object. slice(1) removes the first character which is an extra "
+	beforecomma = beforecomma.replace(/\\/g, ''); //replaces slashes
+	var aftercomma = '{' + splitOn[1].slice(1, -1); // adds { before the second part. Removes first and last characters which are extra "
+	aftercomma = aftercomma.replace(/\\/g, '');  // replaces slashes
+	
+	beforecomma = beforecomma.slice(1,-1);
+	aftercomma = aftercomma.slice(1,-1);
+	
+	var result1parts = beforecomma.split(':');
+	var result2parts = aftercomma.split(':');
 
 	result1parts[0] = result1parts[0].slice(1,-1);
 	result1parts[1] = result1parts[1].slice(1,-1).slice(1);
-	console.log(result1parts);
-
+	
 	result2parts[0] = result2parts[0].slice(1,-1);
 	result2parts[1] = result2parts[1].slice(1,-1).slice(1);
-	console.log(result2parts);
+	/*----------------------------------------------------------*/
+	
 
 
-
-	//console.log(cookie);
-	//console.log(newTemp);
-	// cookie = JSON.parse(cookie);
-	// console.log(cookie);
-	// console.log(Object.values(cookie));
-	//console.log(JSON.parse(cookie));
-	// console.log(JSON.parse(cookie).username);
-	//console.log(JSON.parse(cookie)["group"]);
 	$scope.userInfo = {
 		userName: (result1parts[0] === 'username') ? result1parts[1] : result2parts[1],
 		group: (result1parts[0] === 'group') ? result1parts[1] : result2parts[1]
 	};
-	//console.log(JSON.parse(cookie));
+
+	console.log($scope.userInfo.group);
 }
